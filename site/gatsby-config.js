c4ffc8227fd0d377e1f991c3ac36915a9665cef9
Kawatsu Kenya <3982ne@gmail.com>
2021-08-09 15:24:43 +0900

    fix default settings

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,15 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `webエンジニアの蛙のブログ`,
+    description: `webエンジニアの蛙のブログ`,
+    siteUrl: `https://blog.gekishiman.work`,
   },
   plugins: [
     `gatsby-plugin-image`,
