8668b87cc467537ac3516de67ee6b5fc964f5ee9
Kawatsu Kenya <3982ne@gmail.com>
2021-10-09 16:45:09 +0900

    fix footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        Contact: 3982ne@gmail.com
       </footer>
     </div>
   )
